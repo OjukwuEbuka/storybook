d16c58447c6ec7ade32612374aabfaa1b8591a09
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-09-01 21:53:54 +0100

    Completed Google Oauth setup

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
+const passport = require('passport');
+const path = require('path');
+const session = require('express-session');
 
 // load config
 dotenv.config({path: './config/config.env'});
+
+// passport config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -19,6 +25,26 @@ if(process.env.NODE_ENV === 'development'){
 // Handlebars config
 app.engine('.hbs', exphbs({defaultLayout:'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+// Sessions
+app.use(session({
+    secret: "avenderaldora",
+    resave: false,
+    saveUninitialized: false,
+    // cookie: {secure: true}
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
